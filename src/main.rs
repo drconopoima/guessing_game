482307c70ce23b31efc0512364d4c855640c090a
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-12-07 22:41:10 +0100

    Add input loop to guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Enter a new guess:");
-    let secret_number: usize = rand::thread_rng().gen_range(1, 101);
-    println!("Please type your desired number between 1 and 100: ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to retrieve value");
-    println!("Your guess is: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        println!("Please type your guess between 1 and 100: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to retrieve value");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        println!("Your guess is: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { println!("You win!"); break;},
+        };
+    }
 }
